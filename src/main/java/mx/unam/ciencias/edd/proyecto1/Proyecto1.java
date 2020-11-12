dbb7217f40e4670944b7af056457788f4840d358
garayfernandor <72635259+garayfernandor@users.noreply.github.com>
2020-11-11 21:13:14 -0600

    Update Proyecto1.java

--- src/main/java/mx/unam/ciencias/edd/proyecto1/Proyecto1.java
+++ src/main/java/mx/unam/ciencias/edd/proyecto1/Proyecto1.java
@@ -1,15 +1,6 @@
 package mx.unam.ciencias.edd.proyecto1;
 
-import mx.unam.ciencias.edd.*;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
-import java.io.FileNotFoundException;
+import mx.unam.ciencias.edd.ArbolRojinegro;
 
 /**
  * Proyecto 1: Ordenador lexicográfico.
